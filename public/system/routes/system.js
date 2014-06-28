562bf53966746524583d79f5e908180623133a20
DamTosoni <damtos@gmail.com>
2014-06-28 23:37:29 +0200

    Application structure rethought; improved main page, login page, register page.

--- public/system/routes/system.js
+++ public/system/routes/system.js
@@ -15,6 +15,10 @@ angular.module('mean.system').config(['$stateProvider', '$urlRouterProvider',
                 .state('auth', {
                     templateUrl: 'public/auth/views/index.html'
                 })
+                .state('info', {
+                    url: '/info',
+                    templateUrl: 'public/system/views/info.html'
+                })
                 .state('aboutUs', {
                     url: '/aboutUs',
                     templateUrl: 'public/system/views/aboutUs.html'
