03f24ae1d71627fba39dbafcef922c2b4ee0c2eb
Davide <thekage91@gmail.com>
2014-07-08 17:49:27 +0200

    Perform authorize user page

---
+++ public/dashboard/assets/js/common_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(e){
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("name");
+		var concept = $(this).text();
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
+	});
+});
