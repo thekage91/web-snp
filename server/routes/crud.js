bb6b6a9ac5d8d16412f8596a6c18c2813e56f175
ugo <ugo.buonadonna@stud.uniroma3.it>
2014-07-12 13:56:52 +0200

    CRUD using mers. Added to package

--- server/routes/crud.js
+++ server/routes/crud.js
@@ -1,9 +1,14 @@
 'use strict';
 
 // User routes use dbsnps controller
-var dbsnps = require('../controllers/dbSNP');
 var express = require('express'); 
-
+ var mers = require('mers');
+ 
+ module.exports = function(app, passport) { 
+    app.use('/api', mers({uri:'mongodb://localhost/rest_example_prod'}).rest());
+ };
+    
+/*
 module.exports = function(app, passport) {  
     var router = express.Router();
     // Setting up the dbsnps api
@@ -19,10 +24,9 @@ module.exports = function(app, passport) {
 
        
     app.use('/api', router);
-    /*// AngularJS route to check for authentication
+    // AngularJS route to check for authentication
     app.route('/loggedin')
         .get(function(req, res) {
             res.send(req.isAuthenticated() ? req.user : '0');
         });*/
   
-};
