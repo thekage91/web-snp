8733cd2a357617f8500dfac821e8f5c36b35bdd0
ugo <ugo.buonadonna@stud.uniroma3.it>
2014-07-17 23:44:20 +0200

    Fixed user root seed. Fixed controller family

--- server/config/seed.js
+++ server/config/seed.js
@@ -12,11 +12,15 @@ var User = mongoose.model('User');
 User.findOne({ 'name': 'root' }, 'name occupation', function (err, user) {
   if(!user) {
         var admin = new User ( { name : 'root' ,
+                         firstName : 'root',
+                         lastName : 'root',
                          email : 'root@root.com',
+                         address : 'root street',
+                         phone : '000',
                          username : 'root',
                          roles: ['admin','authenticated','licensed'],
                          password: 'rootadminasd'
                           });
-        admin.save( function (err) { console.log("internal error") }); 
+        admin.save( function (err) { console.log("internal error "+err)  }); 
   }
   } );
